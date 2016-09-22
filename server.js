c8cd653245740fe8df8775bbbc393db8291f9c26
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-22 22:03:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article one is being requested');
+});
+
+app.get('/article-two', function (req, res) {
+  res.send('Article two is being requested');
+});
+
+app.get('/article-three', function (req, res) {
+  res.send('Article three is being requested');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
