8f20217b28bf49bc1b32ec5ad4698452453b1ed7
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-27 01:05:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,64 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne = {
+    title: "Article One | SANJEEV",
+    heading: "Article one",
+    content : `
+            <p>
+                        This is my first page on the article one, learning with IMAD and different ascpets of web technology
+                    </p>
+                    <ol>
+                        <li>JavaScript</li>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>nodejs</li>
+                    </ol>
+                    <p>
+                       In association with IMAD & Hasura.io.
+                    </p>
+    
+    `
+ };
+
+
+function createTemplate (data){
+    var title= data.title;
+    var heading= data.heading;
+    var content= data.content;
+    var htmltemplate=`  <html>
+    <head>
+        ${title} 
+        <meta name="viewport" content="width=device=width,inital-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+     </head>
+     <body>
+         <div class="container">
+                <div>
+                     <a href ="/">HOME</a>
+                </div>
+                <hr/>
+                    ${heading}
+                    ${content}
+        </div>
+     </body>
+</html>`
+
+return htmltemplate
+    
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
+/*app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});*/
+
+app.get('/article-one', function (req, res) {
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
