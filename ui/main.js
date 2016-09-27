018b9ca8d3eefb92188987b61150d101ebdf302d
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-27 16:53:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,17 @@ element.innerHTML = "This is the test ";
 
 //move the image
 var img= document.getElementById("madi");
+
+var marginLeft=0;
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft= marginLeft + 'px';
+}
+    
+
+
 img.onclick= function(){
-    img.style.marginLeft='100px';
+  //  img.style.marginLeft='100px';
+    var interval = setInterval(moveRight,100);
+  
 }
