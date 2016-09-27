e64bd9863e08476e408664f90fb6364b60596ce1
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-27 15:24:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,6 +120,11 @@ app.get('/article-three', function (req, res) {
 });
 
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
