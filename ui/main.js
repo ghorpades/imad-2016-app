76d2bc90fd89a5b1535616fc7647b766bff4d4d6
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-29 17:55:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,9 +3,6 @@ console.log('Loaded!');
 
 //change the text of main div
 
-var element = document.getElementById("maintext");
-element.innerHTML = "This is the test ";
-
 //move the image
 var img= document.getElementById("madi");
 
