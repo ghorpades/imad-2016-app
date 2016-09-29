6b5112111dd4fcc96bb6f8aa8e4e685063aec813
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-29 23:25:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,6 +127,21 @@ app.get('/count', function (req,res){
    res.send(counter.toString());
 });
 
+var names =[];
+app.get('/submit-name', function (req, res) { // jquery /submit?name=xxxx
+  // get the names from the request
+  
+ // var name = req.params.name;
+  
+  var name = req.query.name;
+  names.push(name);
+  
+  //json : java scirpt object notaation.. which converts objects to string
+  
+  res.send(JSON.stringify(names));
+  
+});
+
 app.get('/:name', function (req, res) {
    
    var name = req.params.name;
@@ -142,20 +157,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names =[];
-app.get('/submit-name', function (req, res) { // jquery /submit?name=xxxx
-  // get the names from the request
-  
- // var name = req.params.name;
-  
-  var name = req.query.name;
-  names.push(name);
-  
-  //json : java scirpt object notaation.. which converts objects to string
-  
-  res.send(JSON.stringify(names));
-  
-});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
