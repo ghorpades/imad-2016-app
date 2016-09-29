38a5de63e2b448d7c7076a0594ce9b7cb4877f0d
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-29 23:19:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names =[];
-app.get('/submit-name:/name', function (req, res) {
+app.get('/submit-name/:name', function (req, res) {
   // get the names from the request
   
   var name = req.params.name;
