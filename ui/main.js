0758d4059472b184a15c4520cb375f84d625297b
ghorpades <sanjeev.ghorpade@gmail.com>
2016-09-29 18:09:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,11 +23,30 @@ var count = document.getElementById("count");
 var counter=0;
 var click = document.getElementById("click");
 click.onclick= function(){
-    //make the request to endpiont
     
-    // render the variable.
-    counter= counter+1;
     
-    count.innerHTML=counter.toString();
-}
+    //create the request Object
+    var request = new XMLHttpRequest();
+    
+    // Capture the request 
+    request.onreadystatechange = function(){
+        if(request.readySate === XMLHttpRequest.DONE){
+            // Take some action 
+            if(requesst.status === 200){
+                var counter = request.responseText;
+                 count.innerHTML=counter.toString();
+            }
+            
+        }
+    };
+    
+    
+    /*counter= counter+1;
+    count.innerHTML=counter.toString();*/
+    
+    //Make the request
+    request.Open('GET','http://ghorpades.imad.hasura-app.io/count',true);
+    request.send(null);
+    
+};
     
